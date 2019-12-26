2f127e6d908aae42443a990393a4fa82849a2ee4
Ludmilla Aires Negri <ludaires@gmail.com>
2019-12-26 15:50:20 -0500

    improve code.

--- two-sum.js
+++ two-sum.js
@@ -1,58 +1,28 @@
-// var generate = function(numRows) {
-// ​
-//     let result=[[1]]
-//     if (numRows===0) return [];
-//     if (numRows===1) return result;
-//     result.push([1,1])
-//     if (numRows===2) return result
-    
-//     for (let i=2;i<numRows ;i++){
-//         let last=result[result.length-1]
-//         let temp=[1]
-//        for(let j=0; j< last.length-1 ;j++){
-//            let sum=parseInt(last[j])+ parseInt(last[j+1])
-//            temp.push(sum)
-//        }
-//         temp.push(1)
-//         result.push(temp)
+
+// 120 ms
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             if (nums[j] === (target - nums[i])) {
+//                 return [i, j]; 
+//             } else {
+//                 return 'No two sum solution';
+//             }
+//         }
 //     }
-//     return result
- 
 // };
 
 
-// const reverseString = function(str) {
-//     const newArray = []
-//     for (let i = str.length - 1; i >= 0; i--){
-//         newArray.push(str[i]);
-//     }   
-//     return newArray.toString();
-// }
-
-// console.log(reverseString('Clara'));
-
-var twoSum = function (nums, target) {
-    const cache = {};
+// 52 ms - better solution
+var twoSum = function(nums, target) {
+    const numbers = {}
     for (let i = 0; i < nums.length; i++) {
-        if (!cache[nums[i]]) {
-            cache[nums[i]] = nums[i];
+        if(numbers[target - nums[i]] !== undefined ) {
+            return [numbers[target - nums[i]], i];
         }
+        numbers[nums[i]] = i;
     }
-
-    console.log("cache", cache);
-    
-    for (let i = 0; i < nums.length; i++) {
-        let wishNumber = Math.abs(target - nums[i]);
-        console.log("target", target, "nums[i]", nums[i], "wishnumber", wishNumber, "cache[wishnumber]", cache[wishNumber]);
-        if (wishNumber === cache[wishNumber]) {                
-            return [i, cache[wishNumber]];        
-        }
-    }
-
-    return [];
 };
 
-const nums = [2,7,11,15];
-const result = twoSum(nums, 9);
-console.log(result);
+console.log(twoSum([2, 7, 11, 15], 9));
 
